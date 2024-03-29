b8052bcee5f14806e8e95cb414cf0adddce83130
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-29 16:55:05 -0500

    imported sq thing so it can access a database

--- Login.py
+++ Login.py
@@ -1,9 +1,12 @@
 from tkinter import *
 from tkinter import ttk
+import sqlite3
 
 root = Tk()
 
 
+conn= sqlite3.connect('LoginInfo.db')
+curse = conn.cursor()
 
 
 
--- SignUp.py
+++ SignUp.py
@@ -1,9 +1,13 @@
 from tkinter import *
 from tkinter import ttk
+import sqlite3
 
 root = Tk()
 
 
+
+conn= sqlite3.connect('LoginInfo.db')
+curse = conn.cursor()
 
 
 
