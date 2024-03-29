91e2c3a794a8b56d82a86d40ea5541525a6be5cd
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-29 17:01:48 -0500

    added basic stuff to make a databe for login info and to add the table user info with the attrabutes email and password. also added a function to add to the table using varablies for later use

--- SignUp.py
+++ SignUp.py
@@ -10,5 +10,29 @@ conn= sqlite3.connect('LoginInfo.db')
 curse = conn.cursor()
 
 
+class SignUpTime:
+    def __init__(self, master):
+        self.labe1 = ttk.Label(master)
+
+
+curse.execute("""
+              CREATE TABLE IF NOT EXISTS userInfo (
+              email TEXT,
+              password TEXT,
+              """)
+
+def addUser(email, password):
+
+    curse.execute("""
+                INSERT INTO userInfo(email, password) 
+                VALUES (?,?,?,?)""", (email, password)
+    )
+    conn.commit()
+
+
+
+
+#addUser()
+
 
 root.mainloop()
