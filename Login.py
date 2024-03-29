ebc65d2665345aab6b60c165c513ccc9c4ed0fb3
Kimberlee Finch <kmfinch42@tntech.edu>
2024-03-29 18:43:06 -0500

    ruff draft of login window

--- Login.py
+++ Login.py
@@ -3,10 +3,32 @@ from tkinter import ttk
 import sqlite3
 
 root = Tk()
+root.title("Login Page")
 
 
 conn= sqlite3.connect('LoginInfo.db')
 curse = conn.cursor()
+
+class LoginTime:
+
+    def __init__(self, master):
+        
+        self.labeEmail = ttk.Label(master)
+        self.labeEmail.config(text= "Email: ")
+        self.labeEmail.grid(row=0,column=0)
+
+        self.EmailEntry = ttk.Entry(master)
+        self.EmailEntry.grid(row=0,column=15)
+
+        self.labePass = ttk.Label(master)
+        self.labePass.config(text="Password: ")
+        self.labePass.grid(row=25,column=0)
+
+        self.PassEntry = ttk.Entry(master)
+        self.PassEntry.grid(row=25,column=15)
+
+
+
 
 
 
